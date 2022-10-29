f259f615c743dfe1238a4891289f6a7f35dcd93a
changwoolim <limcw@zarathu.com>
2022-10-29 21:10:05 +0900

    Change HTML/CSS to modal

--- R/use_emailjsr.R
+++ R/use_emailjsr.R
@@ -1,40 +1,83 @@
 #'
 #' @title use_emailjsr
 #' @description emailjs.com R support
-#' @import glue
-#' @import htmltools
+#' @import httr
+#' @import shiny
+#' @importfrom shinybrowser get_width get_height
 #'
 #' @examples
 #' library(emailjsr)
 #'
 #' @rdname use_emailjsr
 #'
+#' @param id Should be same to id of use_emailjsr_server
 #' @param serviceId emailjs.com Service Id
 #' @param userId emailjs.com User Id
 #' @param templateId emailjs.com Template Id
+#' @param access_token emailjs.com Access Token
 #' @export
 
-use_emailjsr <- function(serviceId = NULL, userId = NULL, templateId = NULL) {
-  if (is.null(serviceId) | is.null(userId) | is.null(templateId)) {
-    stop("Param serviceId, userId, templateId is Required.")
-  }
-  return(
-    htmltools::tagList(
-      tags$script(
-        glue::glue(
-          .open = "{{{",
-          .close = "}}}",
-          '
-          const serviceId="{{{serviceId}}}";
-          const templateId="{{{templateId}}}";
-          const userId="{{{userId}}}";
-          '
+use_emailjsr_ui <- function(id) {
+  ns <- NS(id)
+  actionButton(ns("showmodal"), "Show feedback modal")
+}
+
+#' @export
+use_emailjsr_server <- function(id, service_id, user_id, template_id, access_token) {
+  url = "https://api.emailjs.com/api/v1.0/email/send"
+  moduleServer(id, function(input, output, session) {
+    ns <- session$ns
+
+    dataModal <- function(failed = FALSE) {
+      modalDialog(
+        textInput(ns("name"), "Your Name",
+          placeholder = "Your Name"
+        ),
+        textInput(ns("email"), "Your E-mail",
+          placeholder = "Your E-mail"
+        ),
+        textAreaInput(ns("feedback"), "Feedback *",
+          placeholder = "Type down your feedback to developer. (Required)"
+        ),
+        if (failed == TRUE) {
+          span("Feedback is essential.", style = "color: red;")
+        },
+        footer = tagList(
+          modalButton("Cancel"),
+          actionButton(ns("submit"), "Submit")
         )
-      ),
-      includeScript("https://cdn.jsdelivr.net/npm/@emailjs/browser@3/dist/email.min.js"),
-      includeCSS("feedback.css"),
-      includeHTML("feedback.html"),
-      includeScript("www/app.js"),
-    )
-  )
+      )
+    }
+
+    observeEvent(input$showmodal, {
+      showModal(dataModal())
+    })
+
+    observeEvent(input$submit, {
+      if (!(input$feedback == "")) {
+        template_params <- list(
+          fromName = input$name,
+          message = input$feedback,
+          replyTo = input$email,
+          userAgent = tryCatch(getOption("HTTPUserAgent"), error = function(e) ("UserAgent Not Available")),
+          browserWidth = tryCatch(shinybrowser::get_width(), error = function(e) ("Width Not Available")),
+          browserHeight = tryCatch(shinybrowser::get_height(), error = function(e) ("Height Not Available"))
+        )
+        POST(
+          url = url,
+          body = list(
+            service_id = service_id,
+            template_id = template_id,
+            user_id = user_id,
+            accessToken = access_token,
+            template_params = template_params
+          ),
+          encode = "json"
+        )
+        removeModal()
+      } else {
+        showModal(dataModal(failed = TRUE))
+      }
+    })
+  })
 }
